d683123fda519b2a4df37d0eb9a2d124d0e7258c
mitsuhiro-U <mmm.udagawa@gmail.com>
2021-06-06 06:52:54 +0000

    [add]ファイルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/index'
-  get 'homes/show'
-  get 'homes/new'
-  get 'homes/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'books/new'
+  resources :homes
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
